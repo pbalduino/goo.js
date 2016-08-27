d3e6eb7dcb7a5fe9821f58590b551fea577f1b1c
Plinio Balduino <pbalduino+github@gmail.com>
2016-08-27 10:53:20 -0300

    Developing finite lazy seq

--- goo.js
+++ goo.js
@@ -15,27 +15,61 @@ module.exports = (function() {
     };
   };
 
+  var _LazySeq = function(init, thunk) {
+    var _head = new _Node(init, thunk);
+    var _size = 1;
+    var _thunk = thunk;
+
+    this.size = function() {
+      return _size;
+    };
+
+    this.first = function() {
+      return this.get(0);
+    }
+
+    this.rest = function() {
+      return new _LazySeq(this.first(), thunk);
+    };
+
+    this.get = function(pos) {
+      var current = _head;
+      for(var p = 0; p < pos; p++) {
+        current = current === null ? null : current.getNext();
+      }
+      return current === null ? null : current.getValue();
+    }
+  };
+
+  var _rest = function(seq) {
+    return seq.rest();
+  }
+
   var _inc = function(num) {
     return num + 1;
-  }
+  };
 
   var _dec = function(num) {
     return num - 1;
+  };
+
+  var _first = function(seq) {
+    return seq.first();
+  };
+
+  var _get = function(seq, pos) {
+    return seq.get(pos);
   }
 
   return {
     Node: _Node,
 
-    LazySeq: function(init, thunk) {
-      var _head = new Node(init, thunk);
-      var _size = 1;
-
-      this.size = function() {
-        return _size;
-      };
-    },
+    LazySeq: _LazySeq,
+    rest: _rest,
     inc: _inc,
     dec: _dec,
+    first: _first,
+    get: _get,
     map: function(fun, seq) {
 
     },
--- spec/goo_spec.js
+++ spec/goo_spec.js
@@ -6,12 +6,29 @@ describe("Using goo module", function() {
     expect(goo).not.toBe(undefined);
   });
 
-  it("Creating LazySeq with inc", function() {
+  it("Traversing Node with inc", function() {
     var node = new goo.Node(0, goo.inc);
 
     for(var p = 0; p < 10; p++) {
       console.log(node.getValue());
       node = node.getNext();
     };
+  });
+
+  it("Create a LazySeq with inc", function() {
+    var ls = new goo.LazySeq(0, goo.inc);
+
+    for(var p = 0; p < 10; p++) {
+      console.log(ls.get(p));
+    }
+  });
+
+  it("Create a LazySeq with inc - functional", function() {
+    var ls = new goo.LazySeq(0, goo.inc);
+
+    while(goo.first(ls) < 10) {
+      console.log(goo.first(ls));
+      ls = goo.rest(ls);
+    }
   });
 });
